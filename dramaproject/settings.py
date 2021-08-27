3bc658202ca536d9d8b798c3171eeaa76223adfd
chjs918 <zzisoo13@gmail.com>
2021-08-28 02:29:48 +0900

    second commit

--- dramaproject/settings.py
+++ dramaproject/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'webtoon', 'static')
+    os.path.join(BASE_DIR, 'dramaboard', 'static')
 ]
 #현재 static 파일들이 어디에 있는지
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
